b13af3fdf4b3e24943754c6908b0446aa7eb1953
Tor <chayoud@abroaders.jp>
2020-01-21 09:13:57 +0700

    update dockerize

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_095900) do
+ActiveRecord::Schema.define(version: 2020_01_20_092809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
