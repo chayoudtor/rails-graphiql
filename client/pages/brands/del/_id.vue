141454e4697ba7b7dcb0a2ce4a349e9498fb99c9
Chayoud <chayoud@abroaders.jp>
2020-01-23 12:59:28 +0700

    add query singlq role gql

--- client/pages/brands/del/_id.vue
+++ client/pages/brands/del/_id.vue
@@ -5,7 +5,24 @@
 </template>
 
 <script>
+import queryBrand from "~/graphql/query/brand.gql"
 export default {
-  layout: 'form_default'
+  layout: 'form_default',
+  data() {
+    return {
+      brand: []
+    }
+  },
+  apollo: {
+    brand: {
+      prefetch: true,
+      query: queryBrand,
+      valiables () {
+        return {
+          id: parseInt(this.$route.query.id)
+        }
+      }
+    }
+  }
 }
 </script>
